3f19d0d3491ad934a4bc075e06189b85da5a8ad1
theInternauts <cricketw@wearetheinternauts.com>
2014-02-01 02:49:04 -0800

    better send_mail script to do basic validation--error handling could be better

--- scripts/send_mail.php
+++ scripts/send_mail.php
@@ -1,17 +1,47 @@
 <?php
-	$subject = "A message from the Internauts contact form"; 
-	$message = $_POST['message'];
-	$message = wordwrap($message, 70);
-	$name = $_POST['name'];
-	$mail_from = $_POST['email']; 
-	
-	$header = "from: $name <$mail_from>";
-	$to ='cricketw@WeAreTheInternauts.com';
-	$send_contact = mail($to,$subject,$message,$header);
+	function spamcheck($field)
+	{
+	  //filter_var() sanitizes the e-mail
+	  //address using FILTER_SANITIZE_EMAIL
+		$field=filter_var($field, FILTER_SANITIZE_EMAIL);
 
-	if($send_contact == true){
-		echo "We've recieved your contact information";
+	  //filter_var() validates the e-mail
+	  //address using FILTER_VALIDATE_EMAIL
+		if(filter_var($field, FILTER_VALIDATE_EMAIL))
+		{
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+
+	//if "email" is filled out, proceed
+	if (isset($_REQUEST['email']))
+	{
+		//check if the email address is invalid
+		$mailcheck = spamcheck($_REQUEST['email']);
+		if ($mailcheck==FALSE)
+		{
+	    	echo "Invalid input";
+	    } else {
+	    	//send email
+			$subject = "A message from the Internauts contact form"; 
+			$message = $_POST['message'];
+			$message = wordwrap($message, 70);
+			$name = $_POST['name'];
+			$mail_from = $_POST['email']; 
+			
+			$header = "from: $name <$mail_from>";
+			$to ='cricketw@WeAreTheInternauts.com';
+			$send_contact = mail($to,$subject,$message,$header);
+
+			if($send_contact == true){
+				echo "We've recieved your contact information";
+			} else {
+				echo "SEND ERROR";
+			}
+	    }
 	} else {
-		echo "ERROR";
+		echo "please fill in all fields";
 	}
 ?>
