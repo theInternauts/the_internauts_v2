9b063da72a5ee9ca2e108ff8a9c49666879354a5
theInternauts <cricketw@wearetheinternauts.com>
2014-02-01 02:31:02 -0800

    bug fix reformat of the mail script.

--- scripts/send_mail.php
+++ scripts/send_mail.php
@@ -1,15 +1,15 @@
 <?php
 	$subject = "A message from the Internauts contact form"; 
-	$message = "$message";
-	$name = "$name";
-	$mail_from = "$email"; 
+	$message = $_POST['message'];
+	$message = wordwrap($message, 70);
+	$name = $_POST['name'];
+	$mail_from = $_POST['email']; 
 	
 	$header = "from: $name <$mail_from>";
-	$to ='cricketw@WeAreTheInternauts.com'
-
+	$to ='cricketw@WeAreTheInternauts.com';
 	$send_contact = mail($to,$subject,$message,$header);
 
-	if($send_contact){
+	if($send_contact == true){
 		echo "We've recieved your contact information";
 	} else {
 		echo "ERROR";
