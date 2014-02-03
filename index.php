a266fe659ef50a3a7be492bcd42dfcb43451bd8c
theInternauts <cricketw@wearetheinternauts.com>
2014-02-03 05:23:07 -0800

    updating the page title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 -->
 <html>
 	<head>
-		<title>Overflow by HTML5 UP</title>
+		<title>Cricket Wallace</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
